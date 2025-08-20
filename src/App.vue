9d1528805125cedcc2fa175e3e5a855e3824684e
unlimmitted <kosnmasss@gmail.com>
2025-08-20 22:32:33 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-container">
+    <div class="main-frame">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+}
+.main-frame {
+  display: flex;
+  border-radius: 15px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: black;
+  padding: 10px;
+}
+</style>
---
+++ src/components/new-duty/ChooseContact.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "ChooseContact"
+})
+</script>
+
+<style scoped>
+
+</style>
